3c8a6cf77531d18c3dbe2b61f90233b7ecbc86c1
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2021-08-28 06:41:59 +0700

    customer address feature

--- src/main/java/com/simple/pos/simplepointofsale/controller/CustomerAddressesController.java
+++ src/main/java/com/simple/pos/simplepointofsale/controller/CustomerAddressesController.java
@@ -1,5 +1,6 @@
 package com.simple.pos.simplepointofsale.controller;
 
+import java.text.ParseException;
 import java.util.Date;
 
 import com.simple.pos.simplepointofsale.Dto.CustomerAddressesDto;
@@ -14,9 +15,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
- 
+  
 @Controller
 @RequestMapping("/customer-addresses")
 public class CustomerAddressesController {
@@ -75,6 +77,65 @@ public class CustomerAddressesController {
             dateTo
         ));
 
+        return "redirect:/customer-addresses/list";
+    }
+  
+    @GetMapping("/update-form/{id}")
+    public String updateFormCustomerAddress(
+        @PathVariable Long id,
+        Model model
+    ){
+        CustomerAddresses customerAddresses = customerAddressService.getCustomerAddressById(id);
+
+        String dateFrom = converterService.dateToString(customerAddresses.getDateFrom(), "yyyy-MM-dd");
+        String dateTo = converterService.dateToString(customerAddresses.getDateTo(), "yyyy-MM-dd");
+
+        CustomerAddressesDto customerAddressesDto = new CustomerAddressesDto(
+            customerAddresses.getCustomerId(),
+            customerAddresses.getAddressId(),
+            customerAddresses.getAddressTypeCode(),
+            dateFrom,
+            dateTo
+        );
+
+        model.addAttribute("updateFormLink", updateFormLink + '/' + id);
+        model.addAttribute("listLink", listLink);
+        model.addAttribute("titleCRUD", titleCRUD);
+        model.addAttribute("customerAddressesDto", customerAddressesDto);
+        model.addAttribute("customerAddressesId", id);
+
+        return "customer_address_ui/update_customer_addresses";
+    }
+
+    @PostMapping("/update-form/{id}")
+    public String updateCustomerAddress(
+        @PathVariable(value = "id") Long id,
+        @ModelAttribute("customerAddressDto") CustomerAddressesDto customerAddressesDto
+    ) throws ParseException{
+        
+        Date dateFrom = converterService.stringToDate(customerAddressesDto.getDateFrom(), "yyyy-MM-dd");
+        Date dateTo = converterService.stringToDate(customerAddressesDto.getDateTo(), "yyyy-MM-dd");
+
+        CustomerAddresses customerAddresses = new CustomerAddresses(
+            customerAddressesDto.getCustomerId(),
+            customerAddressesDto.getAddressId(),
+            customerAddressesDto.getAddressTypeCode(),
+            dateFrom,
+            dateTo
+        );
+
+        customerAddresses.setCustomerAddressId(id);
+
+        customerAddressService.saveCustomerAddress(customerAddresses);
+
+        return "redirect:/customer-addresses/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCustomerAddress(
+        @PathVariable(value = "id") Long id
+    ){
+        this.customerAddressService.deleteCustomerAddressById(id);
         return "redirect:/customer-addresses/list";
     }
 }

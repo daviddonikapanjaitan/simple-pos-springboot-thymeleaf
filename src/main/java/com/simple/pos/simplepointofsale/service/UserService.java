c7d24e0ddab9688bce350ac3fe29c595b878b7cd
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2021-08-31 21:39:10 +0700

    login page on progress

---
+++ src/main/java/com/simple/pos/simplepointofsale/service/UserService.java
@@ -0,0 +1,11 @@
+package com.simple.pos.simplepointofsale.service;
+
+import com.simple.pos.simplepointofsale.Dto.UserRegistrationDto;
+import com.simple.pos.simplepointofsale.model.User;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService{
+    
+    User save(UserRegistrationDto registrationDto);
+}

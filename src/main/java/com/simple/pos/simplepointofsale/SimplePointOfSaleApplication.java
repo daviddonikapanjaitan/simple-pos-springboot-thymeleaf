4878e6694927c16da8c9f36e3cc8dbb0912dc4fd
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2021-08-31 20:14:14 +0700

    login page on progress

--- src/main/java/com/simple/pos/simplepointofsale/SimplePointOfSaleApplication.java
+++ src/main/java/com/simple/pos/simplepointofsale/SimplePointOfSaleApplication.java
@@ -2,7 +2,7 @@ package com.simple.pos.simplepointofsale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class SimplePointOfSaleApplication {
  

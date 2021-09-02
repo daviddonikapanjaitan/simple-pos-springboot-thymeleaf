dc3ef4d60a3880360c3728f78da94927df92781e
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2021-09-02 21:53:13 +0700

    completed address navbar

--- src/main/java/com/simple/pos/simplepointofsale/controller/SupplierLocationsController.java
+++ src/main/java/com/simple/pos/simplepointofsale/controller/SupplierLocationsController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-     
+ 
 @Controller
 @RequestMapping("/supplier-location")
 public class SupplierLocationsController {

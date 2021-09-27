d6b2940790ec46fc374d4751c9130dd96b0af617
daviddonikapanjaitan <daviddonikapanjaitan@gmail.com>
2021-09-27 20:11:00 +0700

    cleaning more code

--- src/main/java/com/simple/pos/simplepointofsale/controller/ProductTypesController.java
+++ src/main/java/com/simple/pos/simplepointofsale/controller/ProductTypesController.java
@@ -15,9 +15,7 @@ import com.simple.pos.simplepointofsale.validationService.ProductTypesServiceVal
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
